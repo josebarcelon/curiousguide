75d5594dc8f701f5ebb2b250f2714284e07f097e
José Barcelon Godfrey <jose.barcelon@gmail.com>
2021-01-24 19:33:55 +0100

    feat: update footer url

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://twitter.com/jobarcelon"
           target="_blank"
           rel="noopener noreferrer"
         >
