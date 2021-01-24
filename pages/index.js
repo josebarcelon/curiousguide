b2306fd1ab2a1ef9cd85ed526889007a199e216f
José Barcelon Godfrey <jose.barcelon@gmail.com>
2021-01-24 23:33:59 +0100

    feat(landing): update copy

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="font-sans h-screen flex flex-col align-center justify-center">
       <Head>
         <title>Curious Guide | A guide to mental health</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico?v=2" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          🧠 Curious Guide
+      <main className="text-center flex flex-col align-center flex-1 justify-center">
+        <h1 className="text-3xl font-extrabold text-gray-900 tracking-tight">
+          🧠 <span className="bg-pink-100">Curious Guide</span>
         </h1>
-
-        <p className={styles.description}>
-          A guide to mental health
+        <p className="text-1xl text-gray-600 mt-4">A guide to mental health.</p>
+        <p className="text text-gray-400 mt-4">
+          Understanding feeling.
+          <br />
+          Cultivating happiness.
+          <br />
+          Building meaningful relationships.
         </p>
-  
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="text-center p-4 border-t border-gray-300">
+        2021 |{" "}
         <a
           href="https://twitter.com/jobarcelon"
           target="_blank"
@@ -30,5 +33,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
